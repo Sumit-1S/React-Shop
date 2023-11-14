21a462b6b02d4ae91dbe04d3729fbffb342d741d
= <=>
2023-11-13 22:23:37 -0800

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {createStore, compose, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './redux/reducers';
+
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const shopStore = createStore(rootReducer,composeEnhancers(applyMiddleware(thunk)))
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+  <Provider store={shopStore}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/redux/actions/productAction.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { ActionTypes } from "../constants/action-type"
+import fakeStoreApi from "../../APIs/fakeStoreApi";
+
+export const fetchProducts = () => async(dispatch) =>{
+    // return async function (dispatch,getState){
+        const response = await fakeStoreApi('/products')
+        dispatch({type:ActionTypes.FETCH_PRODUCTS,payload:response.data})
+    // }
+}
+export const setProducts = (products)=>{
+    return{
+        type:ActionTypes.SET_PRODUCTS,
+        payload:products
+    }
+}
+export const selectedProducts = (product)=>{
+    return{
+        type:ActionTypes.SELECTED_PRODUCT,
+        payload:product
+    }
+}
